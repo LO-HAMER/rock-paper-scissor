c15a8e29b2e6b6bfe04b8e6afec02d0ccd7252e9
LOHAMER <130869756+LO-HAMER@users.noreply.github.com>
2023-12-29 01:19:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -20,6 +20,65 @@ const getUserChoice = userInput => {
       return 'scissors'
     }
     };
+    const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+     return userInput
+    } else {
+      console.log('Error, please type a other userInput.');
+    }
+    };
+    
+    const getComputerChoice = () => {
+      const randomNumber = Math.floor(Math.random() * 3)
+        switch(randomNumber) {
+       case 0:
+       return 'rock';
+        break;
+        case 1:
+         return 'paper';
+         break;
+         case 2:
+        return 'scissors';
+    }
+    };
+    
+    const determineWinner = (userChoice,computerChoice) => {
+      if (userChoice === computerChoice)
+      {return 'the game was tie.'}
+    
+      if (userChoice === 'rock')
+      { if (computerChoice ==='paper')
+      {return 'the computer won';}
+      } 
+      else { return 'you won';}
+    
+      if (userChoice === 'paper') {
+        if (computerChoice === 'scissors')
+        {return 'computer won';} else {
+          return 'you won';
+        }
+      }
+    
+      if (userChoice === 'scissors') {
+        if (computerChoice === 'rock')
+        {return 'computer won';} else {
+          return 'you won';
+        }
+      }
+    }; 
+    
+    const playGame = () => {
+      const userChoice = getUserChoice('rock');
+      const computerChoice = getComputerChoice();
+      console.log ('you threw:'+ userChoice);
+    console.log ('you computer:'+ computerChoice);
+    console.log (determineWinner(userChoice, computerChoice));
+    }
+    
+    playGame()
+    
+    
     
     console.log(getUserChoice('rock'));
     console.log(getUserChoice('computer'));
